54aaf8255777c80b3f415324b03f62bb8bde0d18
Klent Fermano <klentf1223@gmail.com>
2022-10-26 14:17:01 +0800

    patient update

--- layouts/sidebar-nav.php
+++ layouts/sidebar-nav.php
@@ -1,43 +1,28 @@
 <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-white">
     <div class="position-sticky">
-    <div class="list-group list-group-flush mx-3 mt-4">
-        <a
-        href="#"
-        class="list-group-item list-group-item-action py-2 ripple"
-        aria-current="true"
-        >
-        <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>Main dashboard</span>
-        </a>
-        <a href="#" class="list-group-item list-group-item-action py-2 ripple active">
-        <i class="fas fa-chart-area fa-fw me-3"></i><span>Webiste traffic</span>
-        </a>
-        <a href="#" class="list-group-item list-group-item-action py-2 ripple"
-        ><i class="fas fa-lock fa-fw me-3"></i><span>Password</span></a
-        >
-        <a href="#" class="list-group-item list-group-item-action py-2 ripple"
-        ><i class="fas fa-chart-line fa-fw me-3"></i><span>Analytics</span></a
-        >
-        <a href="#" class="list-group-item list-group-item-action py-2 ripple">
-        <i class="fas fa-chart-pie fa-fw me-3"></i><span>SEO</span>
-        </a>
-        <a href="#" class="list-group-item list-group-item-action py-2 ripple"
-        ><i class="fas fa-chart-bar fa-fw me-3"></i><span>Orders</span></a
-        >
-        <a href="#" class="list-group-item list-group-item-action py-2 ripple"
-        ><i class="fas fa-globe fa-fw me-3"></i><span>International</span></a
-        >
-        <a href="#" class="list-group-item list-group-item-action py-2 ripple"
-        ><i class="fas fa-building fa-fw me-3"></i><span>Partners</span></a
-        >
-        <a href="#" class="list-group-item list-group-item-action py-2 ripple"
-        ><i class="fas fa-calendar fa-fw me-3"></i><span>Calendar</span></a
-        >
-        <a href="#" class="list-group-item list-group-item-action py-2 ripple"
-        ><i class="fas fa-users fa-fw me-3"></i><span>Users</span></a
-        >
-        <a href="#" class="list-group-item list-group-item-action py-2 ripple"
-        ><i class="fas fa-money-bill fa-fw me-3"></i><span>Sales</span></a
-        >
-    </div>
+        <div class="list-group list-group-flush mx-3 mt-4">
+            <a class="text-center" href="#">
+                <img src="<?php echo $root; ?>/img/top-logo-main.png" height="35" alt="TelevisitMD Logo"
+                    loading="lazy" />
+            </a>
+
+            <h5>FILTERS</h5>
+            <h6>States</h6>
+
+            <ul class="list-group list-group-light">
+                <li class="list-group-item border-0 pb-1"><input type="checkbox"/>&nbsp;Alabama</li>
+                <li class="list-group-item border-0 pb-1"><input type="checkbox"/>&nbsp;Alaska</li>
+                <li class="list-group-item border-0 pb-1"><input type="checkbox"/>&nbsp;A third item</li>
+                <li class="list-group-item border-0 pb-1"><input type="checkbox"/>&nbsp;Arizona</li>
+                <li class="list-group-item border-0 pb-1"><input type="checkbox"/>&nbsp;Arkansas</li>
+                <li class="list-group-item border-0 pb-1"><input type="checkbox"/>&nbsp;California</li>
+                <li class="list-group-item border-0 pb-1"><input type="checkbox"/>&nbsp;Colorado</li>
+                <li class="list-group-item border-0 pb-1"><input type="checkbox"/>&nbsp;Connecticut</li>
+                <li class="list-group-item border-0 pb-1"><input type="checkbox"/>&nbsp;Delaware</li>
+                <li class="list-group-item border-0 pb-1"><input type="checkbox"/>&nbsp;District of Columbia</li>
+                <li class="list-group-item border-0 pb-1"><input type="checkbox"/>&nbsp;Florida</li>
+            </ul>
+
+        </div>
     </div>
 </nav>
